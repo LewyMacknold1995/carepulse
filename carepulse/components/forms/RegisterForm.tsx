1ab31600b3c2cf09659bfb8d16a7cd3504fb0405
Lewy Macknold <lewymacknoldseo@gmail.com>
2024-08-29 14:07:15 +0100

    added address form element

--- carepulse/components/forms/RegisterForm.tsx
+++ carepulse/components/forms/RegisterForm.tsx
@@ -139,6 +139,25 @@ export const RegisterForm = ({user} : {user: User }) => {
             />
           </div>
 
+          {/* Address & Occupation */}
+            <div className="flex flex-col gap-6 xl:flex-row">
+            <CustomFormField
+              fieldType={FormFieldType.INPUT}
+              control={form.control}
+              name="address"
+              label="Address"
+              placeholder="14 street, New york, NY - 5101"
+            />
+
+            <CustomFormField
+              fieldType={FormFieldType.INPUT}
+              control={form.control}
+              name="occupation"
+              label="Occupation"
+              placeholder=" Software Engineer"
+            />
+          </div>
+
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
