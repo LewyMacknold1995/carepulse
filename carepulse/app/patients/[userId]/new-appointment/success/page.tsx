d2f9a50ea59e00509af8b1d94ec034a69e42f27f
Lewy Macknold <lewymacknoldseo@gmail.com>
2024-09-05 21:08:54 +0100

    added logo in middle of screen

--- carepulse/app/patients/[userId]/new-appointment/success/page.tsx
+++ carepulse/app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Success = () => {
   return (
-    <div>Success</div>
+    <div className="flex h-screen max-h-screen px-[5%]">
+        <div className="success-img">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="h-10 w-fit"
+          />
+        </Link>
+
+        </div>
+    </div>
   )
 }
 
