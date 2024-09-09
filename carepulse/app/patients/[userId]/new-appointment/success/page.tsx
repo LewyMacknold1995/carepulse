8f4a5e27e4500fd3ad0874a84106fc839246e3e0
Lewy Macknold <lewymacknoldseo@gmail.com>
2024-09-09 19:15:17 +0100

    added success page content

--- carepulse/app/patients/[userId]/new-appointment/success/page.tsx
+++ carepulse/app/patients/[userId]/new-appointment/success/page.tsx
@@ -16,6 +16,20 @@ const Success = () => {
           />
         </Link>
 
+        <section className="flex flex-col items-center">
+          <Image
+            src="/assets/gifs/success.gif"
+            height={300}
+            width={280}
+            alt="success"
+          />
+          <h2 className="header mb-6 max-w-[600px] text-center">
+            Your <span className="text-green-500">appointment request</span> has
+            been successfully submitted!
+          </h2>
+          <p>We&apos;ll be in touch shortly to confirm.</p>
+        </section>
+
         </div>
     </div>
   )
