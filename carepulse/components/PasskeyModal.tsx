00d9a059ca2ed5f9918d883b4a7aacdb701fdef7
Lewy Macknold <lewymacknoldseo@gmail.com>
2024-09-11 21:38:37 +0100

    set up modal

--- carepulse/components/PasskeyModal.tsx
+++ carepulse/components/PasskeyModal.tsx
@@ -1,3 +1,6 @@
+'use client'
+import Image from "next/image";
+
 import {
   Dialog,
   DialogContent,
@@ -6,14 +9,31 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const PasskeyModal = () => {
+    const router = useRouter();
+    const [open, setOpen] = useState(true)
+    const closeModal = () => {
+        setOpen(false);
+        router.push('/')
+    }
+
   return (
-    <Dialog>
-      <DialogTrigger>Open</DialogTrigger>
-      <DialogContent>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="shad-alert-dialog">
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle className="flex items-start justify-between">Admin Access Verification
+            <Image
+              src="/assets/icons/close.svg"
+              alt="close"
+              width={20}
+              height={20}
+              onClick={() => closeModal()}
+              className="cursor-pointer"
+            />
+          </DialogTitle>
           <DialogDescription>
             This action cannot be undone. This will permanently delete your
             account and remove your data from our servers.
