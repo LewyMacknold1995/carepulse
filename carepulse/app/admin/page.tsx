ed83a6a0bbdaca34805220e438a7c9dde154141b
Lewy Macknold <lewymacknoldseo@gmail.com>
2024-09-16 20:01:26 +0100

    first section

--- carepulse/app/admin/page.tsx
+++ carepulse/app/admin/page.tsx
@@ -17,6 +17,16 @@ const admin = () => {
         </Link>
         <p className="text-16-semibold">Admin Dashboard</p>
         </header>
+
+        <main className="admin-main">
+        <section className="w-full space-y-4">
+          <h1 className="header">Welcome 👋</h1>
+          <p className="text-dark-700">
+            Start the day with managing new appointments
+          </p>
+        </section>
+
+        </main>
     </div>
   )
 }
