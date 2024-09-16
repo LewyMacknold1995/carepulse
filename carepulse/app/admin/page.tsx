f0bb1065f1c4ecccb7b95ab537f6dc7f9ffddcad
Lewy Macknold <lewymacknoldseo@gmail.com>
2024-09-16 19:47:25 +0100

    header for admin dashboard

--- carepulse/app/admin/page.tsx
+++ carepulse/app/admin/page.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const admin = () => {
   return (
-    <div>admin</div>
+    <div className="mx auto flex max-w-7xl flex-col space-y-14">
+        <header className="admin-header">
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={32}
+            width={162}
+            alt="logo"
+            className="h-8 w-fit"
+          />
+        </Link>
+        <p className="text-16-semibold">Admin Dashboard</p>
+        </header>
+    </div>
   )
 }
 
